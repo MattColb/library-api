a244df5595344caca567504b202ccd0751a9172d
MattColb <matt@4colberts.com>
2024-01-17 10:39:04 -0600

    Finished with Basic API

--- app.py
+++ app.py
@@ -3,43 +3,86 @@ import mongo_interaction
 
 app = Flask("__name__")
 
-# Used for extracting info from the call
-def extract_info(info):
-    name = info['name']
-    title = info['title']
-    other = {k:v for k,v in info.items() if k not in ['name', 'title']}
-    return name, title, other
 
-@app.route("/", methods=['POST', "GET"])
-def test():
+@app.route("/", methods=['GET'])
+def home():
+    if request.method == "GET":
+        try:
+            return "BAIS:3400 API for student learning materials recommendations"
+        except:
+            return "Error", 400
+    else:
+        return "Method is not available", 405
+
+
+#Adjust the post requests so that if it is invalid, it wont
+@app.route("/material", methods=["GET", "POST"])
+def material():
     if request.method == "POST":
         try:
             info = request.get_json()
-            name, title, other = extract_info(info)
-            id = mongo_interaction.insert_recommendation(name, title, other)
-            return f"Your book is now in the database with ID: {id}. You can search for it with a GET request by either your name or id."
+            id = mongo_interaction.insert_recommendation(info)
+            if id == False:
+                return "Please ensure that you have a 'title' and 'name' in your json to insert into the database.\nAlso, make sure that you are not changing 'date_added' and '_id'"
+            return f"Your book is now in the database with ID: {id}.\nYou can search for it with a GET request by either your name or id."
         except:
             return f"Error", 400
-        
     elif request.method == "GET":
+        ret = mongo_interaction.get_all_recommendations()
+        return ret
+    else:
+        return "Method is not available", 405
+
+
+@app.route("/material/<id>", methods = ["GET", "PUT", "DELETE"])
+def id_material(id):
+    if request.method == "GET":
         try:
-            info = request.get_json()
-            if info.get("id", False):
-                id = info['id']
-                result = mongo_interaction.get_recommendation_by_id(id)
-                ret = f"ObjectID: {id}\n{result['name']}'s favorite book was {result['title']} as of {result['date_added']}\nHere is other information:{result['other']}"
+            ret = mongo_interaction.get_recommendation_by_id(id)
+            return ret
+        except:
+            "Error", 400
+    
+    #Updates it and returns that the id has been updated
+    elif request.method == "PUT":
+        info = request.get_json()
+        result = mongo_interaction.put_by_id(id, info)
+        if not result:
+            return "Please do not change the '_id'."
+        return result
+    
+    elif request.method == "DELETE":
+        try:
+            ret = mongo_interaction.delete_by_id(id)
+            return ret
+        except:
+            return "Error", 400
+    else:
+        return "Method is not available", 405
+
+
+@app.route("/material/search", methods=["GET"])
+def test():
+    if request.method == "GET":
+        try:
+            #Checks if id is in args list
+            if request.args.get("id", False):
+                id = request.args.get("id", "")
+                ret = mongo_interaction.get_recommendation_by_id(id)
                 return ret
-            elif info.get("name", False):
-                name = info["name"]
+            #Checks if name is in args list
+            elif request.args.get("name", False):
+                name = request.args.get("name", "")
                 results = mongo_interaction.get_recommendations_by_name(name)
                 return results
+            #Makes them search by either name or id if it is not in the list.
             else:
                 return "Please search by either the name or id."
         except:
             return f"Error", 400
-        
     else:
         return "Method not currently available", 405
+
 
 if __name__ == "__main__":
     app.run()
--- mongo_interaction.py
+++ mongo_interaction.py
@@ -3,8 +3,15 @@ import datetime
 import certifi
 import json
 from bson import ObjectId
+from bson.json_util import dumps, loads
 import os
 from dotenv import load_dotenv
+
+"""
+TODO:
+Make sure that the delete returns are something that is handled well.
+
+"""
 
 def mongo_connect():
     mongo = os.getenv("MONGO")# Mongo URL
@@ -19,17 +26,14 @@ def get_all_recommendations():
     conn, client = mongo_connect()
 
     all_recommendations = conn.find()  # get all recommendations
+    ret = []
 
-    print("--------------------")
-
-    for doc in all_recommendations:
-        print(doc)
-
-    print("--------------------")
+    for r in all_recommendations:
+        ret.append(r)
 
     client.close()
 
-    return all_recommendations
+    return dumps(ret)
 
 
 def get_recommendations_by_name(name):
@@ -38,13 +42,13 @@ def get_recommendations_by_name(name):
     results = conn.find({"name":name})
 
     #Handling the return in here so that you don't have to deal with it in the main.
-    ret = ""
+    ret = []
     for r in results:
-        ret = ret + f"ObjectID: {r['_id']}\n{r['name']}'s favorite book was {r['title']} as of {r['date_added']}\nHere is other information:{r['other']}\n" + "\n"
+        ret.append(r)
 
     client.close()
 
-    return ret
+    return dumps(ret)
 
 def get_recommendation_by_id(id):
     
@@ -55,29 +59,74 @@ def get_recommendation_by_id(id):
 
     client.close()
     
-    return result
+    return dumps(result)
 
 
-def insert_recommendation(name, title, other):
-    new_recommendation = {
-        "name": name,
-        "title": title,
-        "date_added": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "other":other
-    }
-
+def put_by_id(id, info):
     conn, client = mongo_connect()
 
-    result = conn.insert_one(new_recommendation)
+    filter = {"_id":ObjectId(id)}
+
+    info["date_added"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    updates = {"$set":info}
+
+    if "_id" in info.keys():
+        return False
+
+    result = conn.update_one(filter, updates)
+
+    client.close()
+    
+    return f"Successfully updated ID: {id}"
+
+
+def delete_by_id(id):
+    conn, client = mongo_connect()
+
+    result = conn.delete_one({"_id":ObjectId(id)})
+
+    client.close()
+
+    if result.deleted_count > 0:
+        return f"Successfully deleted the ID: {id}"
+    else:
+        return "The ID was not successfully deleted."
+    
+
+def name_title_check(info):
+    if info.get("name", False) and info.get("title", False):
+        return True
+    else:
+        return False
+
+def insert_recommendation(info):
+    #Error checking: Making sure there is a 'name' and 'title'. Also that '_id' and 'date_added' are not touched. Won't continue if errors
+    if not name_title_check(info):
+        return False
+    if info.get("_id", False):
+        return False
+    if info.get("date_added", False):
+        return False
+
+    #Adds date to their information
+    info["date_added"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    conn, client = mongo_connect()
+
+    #Pulls out id and returns it to user
+    result = conn.insert_one(info)
     id = result.inserted_id
     client.close()
 
     return id
 
 def main():
-    #get_all_recommendations()
+    print(get_all_recommendations())
+    #print(type(get_all_recommendations()))
 
-    get_recommendation_by_id("65a5778354a7d432fc515c07")
+    #print(get_recommendation_by_id("65a5778354a7d432fc515c07"))
+
+    #print(get_recommendations_by_name("Matt Colbert"))
 
     #result = insert_recommendation("Matt Colbert", "Testing2", {"Testing1":"FunctionalityA"})
     #print(result)
